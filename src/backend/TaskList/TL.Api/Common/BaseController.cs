9178b603de2c4abbfc8a014885ae7b5c8f8dc492
Alexander Ashevsky <alexander.ashevsky@intravision.ru>
2024-07-27 15:18:36 +0300

    Updated controllers

--- src/backend/TaskList/TL.Api/Common/BaseController.cs
+++ src/backend/TaskList/TL.Api/Common/BaseController.cs
@@ -1,11 +1,16 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TL.Api.Common
 {
-    [Route("api/[controyyller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class BaseController : ControllerBase
     {
+        protected ISender _sender;
+        public BaseController()
+        {
+            _sender = HttpContext.RequestServices.GetRequiredService<ISender>();
+        }
     }
 }
--- src/backend/TaskList/TL.Api/Controllers/Authentication/PermissionsController.cs
+++ src/backend/TaskList/TL.Api/Controllers/Authentication/PermissionsController.cs
@@ -1,8 +1,38 @@
-﻿using TL.Api.Common;
+﻿using Microsoft.AspNetCore.Mvc;
+using TL.Api.Common;
 
 namespace TL.Api.Controllers.Authentication
 {
     public class PermissionsController : BaseController
     {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetById()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete()
+        {
+            return Ok();
+        }
     }
 }
