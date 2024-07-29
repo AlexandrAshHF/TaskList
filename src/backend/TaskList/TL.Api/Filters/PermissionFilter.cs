4930b28bafb9e1c8cb9a1c670970f37cb55a033e
Alexander Ashevsky <alexander.ashevsky@intravision.ru>
2024-07-29 22:14:43 +0300

    Добавил фильтры-заглушки

---
+++ src/backend/TaskList/TL.Api/Filters/PermissionFilter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using TL.Infrastructure.Abstractions.Providers;
+
+namespace TL.Api.Filters
+{
+    public class PermissionFilter : Attribute, IAsyncResourceFilter
+    {
+        private readonly IPermissionProvider _permissionProvider;
+        private readonly IUserProvider _userProvider;
+
+        public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
+        {
+            await next();
+        }
+    }
+}
---
+++ src/backend/TaskList/TL.Infrastructure/Providers/PermissionProvider.cs
@@ -0,0 +1,8 @@
+﻿using TL.Infrastructure.Abstractions.Providers;
+
+namespace TL.Infrastructure.Providers
+{
+    public class PermissionProvider : IPermissionProvider
+    {
+    }
+}
